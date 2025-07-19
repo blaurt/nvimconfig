fbfee222cf86a809fbaf211ca3f6012c959c1048
blaurt <suverov.dmitriy@gmail.com>
2025-07-19 06:37:56 +0000

    fix syntax

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -42,30 +42,30 @@ return {
 
   -- change some telescope options and a keymap to browse plugin files
   {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ 
-          cwd = require("lazy.core.config").options.root 
-          no_ignore=true
-          hidden=true
-        }) end,
-        desc = "Find Plugin File",
-      },
-    },
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    -- stylua: ignore
+    {
+      "<leader>fp",
+      function()
+        require("telescope.builtin").find_files({
+          cwd = require("lazy.core.config").options.root,
+          no_ignore = true,
+          hidden = true,
+        })
+      end,
+      desc = "Find Plugin File",
     },
   },
+  opts = {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+    },
+  },
+}
 
   -- add pyright to lspconfig
   {
